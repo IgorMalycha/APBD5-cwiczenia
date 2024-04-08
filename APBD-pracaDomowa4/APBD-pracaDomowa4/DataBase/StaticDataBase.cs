69a6762db482a4e4b90452a15e161bfc732454af
IgorMalycha <igormalycha@gmail.com>
2024-04-08 19:27:32 +0200

    add classes and controllers

---
+++ APBD-pracaDomowa4/APBD-pracaDomowa4/DataBase/StaticDataBase.cs
@@ -0,0 +1,33 @@
+﻿using APBD_pracaDomowa4.Models;
+
+namespace APBD_pracaDomowa4.DataBase;
+
+public class StaticDataBase
+{
+    public static List<Animal> animals = new List<Animal>()
+    {
+        new Animal(1, "Fred", "kot", 15, "ruda"),
+        new Animal(2, "Fredek", "kot", 11, "czarna"),
+        new Animal(3, "Rex", "pies", 25, "brazowa"),
+        new Animal(4, "krotka", "papuga", 1, "kolorowa")
+    };
+
+    public static List<Visite> visites = new List<Visite>()
+    {
+        new Visite("02.03.2024", animals[0], "odrobaczanie", 50),
+        new Visite("14.01.2024", animals[4], "ostrzenie dzioba", 400)
+    };
+
+    public static Animal getAnimalById(int id)
+    {
+        foreach (Animal animal in animals)
+        {
+            if (animal.Id == id)
+            {
+                return animal;
+            }
+        }
+        return null;
+    }
+    
+}
---
+++ APBD-pracaDomowa4/APBD-pracaDomowa4/Models/Animal.cs
@@ -0,0 +1,19 @@
+﻿namespace APBD_pracaDomowa4.Models;
+
+public class Animal
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string Category { get; set; }
+    public int Weight { get; set; }
+    public string FurColor { get; set; }
+
+    public Animal(int id, string firstName, string category, int weight, string furColor)
+    {
+        Id = id;
+        FirstName = firstName;
+        Category = category;
+        Weight = weight;
+        FurColor = furColor;
+    }
+}
